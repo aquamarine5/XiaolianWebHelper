c99e01c1da0f6bb7884c685c40e47551e3fef638
海蓝咕咕鸽 <50990424+aquamarine5@users.noreply.github.com>
2024-09-21 18:36:29 +0800

    finish standard

--- src/main/java/org/aquarngd/xiaolianwebhelper/XiaolianwebhelperApplication.java
+++ src/main/java/org/aquarngd/xiaolianwebhelper/XiaolianwebhelperApplication.java
@@ -1,9 +1,7 @@
 package org.aquarngd.xiaolianwebhelper;
-
-import com.alibaba.fastjson2.JSON;
+//Mybatis
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
-import org.aquarngd.xiaolianwebhelper.data.WasherDevice;
 import org.aquarngd.xiaolianwebhelper.data.WasherDeviceRepository;
 import org.aquarngd.xiaolianwebhelper.data.WasherStatus;
 import org.slf4j.Logger;
@@ -14,18 +12,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.Type;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.Objects;
 
+@EnableAsync
 @EnableJpaRepositories(basePackages = "org.aquarngd.xiaolianwebhelper.data")
 @EntityScan(basePackages = "org.aquarngd.xiaolianwebhelper.data")
 @EnableScheduling
@@ -41,11 +41,14 @@ public class XiaolianwebhelperApplication {
     RestTemplate restTemplate;
     Logger logger;
 
+    HttpHeaders httpHeaders=null;
+
     public XiaolianwebhelperApplication() {
         logger = LoggerFactory.getLogger(XiaolianwebhelperApplication.class);
         logger.info("XiaolianWebHelper launched.");
 
         restTemplate = new RestTemplate();
+        //restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
         postData = new JSONObject();
         postData.put("residenceId", 1215856);
         postData.put("floorId", 759936);
@@ -57,23 +60,28 @@ public class XiaolianwebhelperApplication {
         postData.put("_mp", "1_1_2");
         postData.put("miniSource", 3);
         postData.put("system", 2);
+
+
     }
 
     public static void main(String[] args) {
         SpringApplication.run(XiaolianwebhelperApplication.class, args);
     }
 
+    @Async
     @Scheduled(cron = "0/10 * 13-23 * * ? ")
     public void refreshWasherDevicesData() {
-        HttpHeaders httpHeaders = getHttpHeaders();
+        if(httpHeaders==null) httpHeaders = getHttpHeaders();
         HttpEntity<JSONObject> httpEntity = new HttpEntity<>(postData, httpHeaders);
-        ResponseEntity<JSONObject> response = restTemplate.postForEntity("https://netapi.xiaolianhb.com/m/net/stu/residence/listDevice", httpEntity, JSONObject.class);
         JSONObject body=null;
-        if(response.getStatusCode()== HttpStatusCode.valueOf(401)){
-            body=loginAndResend();
+        try{
+            body = restTemplate.postForEntity("https://netapi.xiaolianhb.com/m/net/stu/residence/listDevice", httpEntity, JSONObject.class).getBody();
         }
-        else{
-            body=response.getBody();
+        catch (HttpClientErrorException e){
+            if(e.getStatusCode()== HttpStatusCode.valueOf(401)){
+                body=loginAndResend();
+            }
+            logger.info("error: {}",e.getResponseBodyAsString());
         }
         logger.info("post http.");
 
@@ -87,12 +95,70 @@ public class XiaolianwebhelperApplication {
         }
     }
     private JSONObject loginAndResend(){
-        return null;
+        if (!isDatabaseExisted("data")) {
+            jdbcTemplate.execute("""
+                    CREATE TABLE xiaolian.data (
+                    accessToken TEXT NOT NULL,
+                    refreshToken TEXT NOT NULL
+                    ) CHARACTER SET utf8mb4""");
+        }
+        HttpHeaders headers=new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        logger.info("post data: {}",getLoginJsonObject().toJSONString());
+        HttpEntity<JSONObject> httpEntity = new HttpEntity<>(getLoginJsonObject(), headers);
+        ResponseEntity<JSONObject> responseEntity= restTemplate.postForEntity("https://mapi.xiaolianhb.com/mp/login",httpEntity,JSONObject.class);
+        logger.info("try re-login.");
+        JSONObject response=responseEntity.getBody();
+        if(response!=null){
+            logger.info("sql: login response: {}",response.toJSONString());
+            String accessToken=response.getJSONObject("data").getString("accessToken");
+            String refreshToken=response.getJSONObject("data").getString("refreshToken");
+            jdbcTemplate.execute("UPDATE `data` SET accessToken = '"+accessToken+"'");
+            jdbcTemplate.execute("UPDATE `data` SET refreshToken = '"+refreshToken+"'");
+            httpHeaders.set("accessToken",accessToken);
+            httpHeaders.set("refreshToken",refreshToken);
+        }
+        HttpEntity<JSONObject> httpPostEntity = new HttpEntity<>(postData, httpHeaders);
+        ResponseEntity<JSONObject> postResponse = restTemplate.postForEntity("https://netapi.xiaolianhb.com/m/net/stu/residence/listDevice", httpPostEntity, JSONObject.class);
+        return postResponse.getBody();
     }
-    private static HttpHeaders getHttpHeaders() {
+
+    private static JSONObject getLoginJsonObject() {
+        JSONObject loginData=new JSONObject();
+        loginData.put("appList",new JSONArray());
+        loginData.put("mobile","19030827318");
+        loginData.put("password","070304syz");
+        loginData.put("appVersion","1.4.8");
+        loginData.put("system","2");
+        loginData.put("model","DCO-AL00");
+        loginData.put("lon","");
+        loginData.put("appSource","1");
+        loginData.put("systemVersion","12");
+        loginData.put("brand","HUAWEI");
+        loginData.put("uniqueId","8c185942d3b9cf82");
+        loginData.put("lat","");
+        return loginData;
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        if (!isDatabaseExisted("data")) {
+            jdbcTemplate.execute("""
+                    CREATE TABLE xiaolian.data (
+                    accessToken TEXT NOT NULL,
+                    refreshToken TEXT NOT NULL
+                    ) CHARACTER SET utf8mb4""");
+            logger.debug("sql: create data table");
+            jdbcTemplate.execute(String.format("INSERT INTO `data` (accessToken,refreshToken) VALUES ('%s', '%s')",
+                    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1NzQ1ODcwIiwib3MiOiIwIiwiaXNzIjoiaHR0cHM6Ly94aWFvbGlhbi5pbyIsImlhdCI6MTcyNjkwODcyOSwiZXhwIjoxNzI2OTI2NzI5fQ.WK85D2fwxnd6SsWs8KKBKhm75nBki8C7q_Cs331iazIFo6Ji5JxW7lC2wVkBHcd4XoAnFMI9tUWpOI5rXBMVpg",
+                    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1NzQ1ODcwIiwib3MiOiIwIiwiaXNzIjoiaHR0cHM6Ly94aWFvbGlhbi5pbyIsImlhdCI6MTcyNjkwODcyOSwiZXhwIjoxNzI4MjA0NzI5fQ.Bv2wkS8m7O2gHOfDjxFPRHpBgis4KbXO1R-_Kp0ly3ohPjL9hDQWev66_XjGU0DrnS59B5ZWG0MSh7aPi86SBg"));
+
+        }
+        SqlRowSet rs=jdbcTemplate.queryForRowSet("SELECT * FROM `data`");
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set("accessToken", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1NzQ1ODcwIiwib3MiOiIwIiwiaXNzIjoiaHR0cHM6Ly94aWFvbGlhbi5pbyIsImlhdCI6MTcyNjg4NjMzOSwiZXhwIjoxNzI2OTA0MzM5fQ.PT6gZzsPOOxP5Sxz_Jezty1WDJKIL15oivvUCeigT7gwGM28Ubwqso3Eb05PDE1fOIysSTmythpHC5JcWdbPeQ");
-        httpHeaders.set("refreshToken", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1NzQ1ODcwIiwib3MiOiIwIiwiaXNzIjoiaHR0cHM6Ly94aWFvbGlhbi5pbyIsImlhdCI6MTcyNjg4NjMzOSwiZXhwIjoxNzI4MTgyMzM5fQ.rMl9qGaoexv3Njg0y59O9nJVlGMJ64IPtvaNAIGLA6EH795ajpajq2JCLKy66aBJ4jxPHAqaOYvdIKZ6r2HJeg");
+        if(rs.next()){
+            httpHeaders.set("accessToken", rs.getString("accessToken"));
+            httpHeaders.set("refreshToken", rs.getString("refreshToken"));
+        }
         httpHeaders.set("referer", "https://netapi.xiaolianhb.com/2020042916153901/4.9.2.0/m/net/stu/residence/listDevice");
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         return httpHeaders;
@@ -108,45 +174,24 @@ public class XiaolianwebhelperApplication {
                     status INT NOT NULL,
                     lastUsedTime TIMESTAMP NOT NULL
                     ) CHARACTER SET utf8mb4""");
+            logger.info("sql: create xiaolian table");
         }
         SqlRowSet rs = jdbcTemplate.queryForRowSet("SELECT * FROM `1215856` WHERE deviceId = ?", new Object[]{deviceObject.getInteger("deviceId")}, new int[]{Types.INTEGER});
-
-        logger.info("run after select deviceId");
         if (rs.next()) {
             if (WasherStatus.valueOf(rs.getInt("status")) == WasherStatus.NOT_USING &&
                     WasherStatus.valueOf(deviceObject.getInteger("deviceStatus")) == WasherStatus.USING) {
                 jdbcTemplate.execute("UPDATE `1215856` SET lastUsedTime = NOW() WHERE deviceId = " + deviceObject.getInteger("deviceId"));
-                logger.info("run sql update lastUsedTime");
+                logger.info("sql: run sql update lastUsedTime");
             }
             jdbcTemplate.execute("UPDATE `1215856` SET status = " + deviceObject.getInteger("deviceStatus") + " WHERE deviceId = " + deviceObject.getInteger("deviceId"));
-            logger.info("run sql update status");
         } else {
             jdbcTemplate.execute(String.format("INSERT INTO `1215856` (deviceId, location, status, lastUsedTime, displayNo) VALUES (%d, '%s', %d, NOW(), %d)",
                     deviceObject.getInteger("deviceId"),
                     deviceObject.getString("location"),
                     deviceObject.getInteger("deviceStatus"),
                     deviceObject.getInteger("dispNo")));
-            logger.info("run sql insert into.");
+            logger.info("sql: run sql insert into.");
         }
-
-
-//            repository.save(new WasherDevice(
-//                    deviceObject.getInteger("deviceId"),
-//                    deviceObject.getString("location"),
-//                    WasherStatus.valueOf(deviceObject.getInteger("deviceStatus")),
-//                    new Timestamp(System.currentTimeMillis())
-//            ));
-//            logger.debug("create {}", deviceObject.getInteger("deviceId"));
-//        } else {
-//            WasherDevice washerDevice = repository.findById(deviceObject.getInteger("deviceId")).get();
-//            WasherStatus nowStatus = WasherStatus.valueOf(deviceObject.getInteger("deviceStatus"));
-//            if (nowStatus == WasherStatus.USING && washerDevice.status == WasherStatus.NOT_USING) {
-//                washerDevice.lastUsedTime = new Timestamp(System.currentTimeMillis());
-//            }
-//            washerDevice.status = nowStatus;
-//            repository.save(washerDevice);
-//        }
-
     }
 
     private boolean isDatabaseExisted(String id) {
--- src/main/java/org/aquarngd/xiaolianwebhelper/controller/WasherHelper.java
+++ src/main/java/org/aquarngd/xiaolianwebhelper/controller/WasherHelper.java
@@ -6,6 +6,7 @@ import org.aquarngd.xiaolianwebhelper.data.WasherDeviceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class WasherHelper {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    @CrossOrigin(origins = "*")
     @RequestMapping("/wash")
     public String GetWash(){
         JSONObject jsonObject=new JSONObject();
